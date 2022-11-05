6dc3037bc5c6e6d2e2966de338fed525a43c0858
Andrie Firmansyah <andriefirmansyah334@gmail.com>
2022-11-05 22:39:13 +0700

    Login & Registration view

--- coba-laravel/resources/views/partials/navbar.blade.php
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -5,12 +5,25 @@
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link {{ ($active === "Home") ? 'active' : '' }}" href="/">Home</a>
-                    <a class="nav-link {{ ($active === "About") ? 'active' : '' }}" href="/about">About</a>
-                    <a class="nav-link {{ ($active === "Posts") ? 'active' : '' }}" href="/blog">Blog</a>
-                    <a class="nav-link {{ ($active === "Categories") ? 'active' : '' }}" href="/categories">Categories</a>
-                </div>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link {{ ($active === "Home") ? 'active' : '' }}" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ ($active === "About") ? 'active' : '' }}" href="/about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ ($active === "Posts") ? 'active' : '' }}" href="/blog">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ ($active === "Categories") ? 'active' : '' }}" href="/categories">Categories</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a href="/login" class="nav-link {{ ($active === "Login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
 </nav>
