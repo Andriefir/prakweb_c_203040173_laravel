9a1d0e0815229dc345ac6b9a516268c357203c11
Andrie Firmansyah <andriefirmansyah334@gmail.com>
2022-12-11 15:25:13 +0700

    Update & Delete Post

--- coba-laravel/app/Http/Controllers/DashboardPostController.php
+++ coba-laravel/app/Http/Controllers/DashboardPostController.php
@@ -80,7 +80,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -92,7 +95,25 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:225',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validateData = $request->validate($rules);
+
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::where('id', $post->id)
+            ->update($validateData);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been updated!');    
     }
 
     /**
@@ -103,7 +124,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('dashboard/posts')->with('success', 'Post has been delated!');
     }
 
     public function checkSlug(Request $request)
--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -5,12 +5,15 @@
     <div class="row my-3">
         <div class="col-lg-8">
             <h1 class="mb-3">{{ $post->title }}</h1>
-            <a href="/dashboard/posts" class="btn btn-success"> <span data-feather="arrow-left" class="align-text-bottom"></span>Back to All My Post</a>
-            <a href="" class="btn btn-warning"> <span data-feather="edit" class="align-text-bottom"></span>Edit</a>
-            <a href="" class="btn btn-danger"> <span data-feather="x-circle" class="align-text-bottom"></span>Delete</a>
+            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left" class="align-text-bottom"></span> Back to All My Post</a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"> <span data-feather="edit" class="align-text-bottom"></span> Edit</a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger " onclick="return confirm('Are you sure you want to delete this?')"><span data-feather="x-circle" class="align-text-bottom"></span> Delete</button>
+            </form>
 
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
-
 
             <article class="my-3 fs-5">
                 {!! $post->body !!}
