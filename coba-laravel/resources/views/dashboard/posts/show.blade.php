82d508dcdc1ccaec66167ee7652eef3c5d4eb670
Andrie Firmansyah <andriefirmansyah334@gmail.com>
2022-12-11 13:35:57 +0700

    Validation & Insert Post

--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -15,8 +15,6 @@
             <article class="my-3 fs-5">
                 {!! $post->body !!}
             </article>
-
-        <a class="d-block mt-3" href="/blog">back to posts</a>
         </div>
     </div>
 </div>
